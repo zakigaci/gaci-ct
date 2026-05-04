c9b7e9bf124bc2284b8a20f0d23b6784757d83b8
zakigaci <zakigacigaci@gmail.com>
2026-05-04 13:22:49 +0100

    add galwry section

--- gaci-ct/src/app/page.tsx
+++ gaci-ct/src/app/page.tsx
@@ -3,6 +3,7 @@ import Hero from '@/components/Hero';
 import About from '@/components/About';
 import Services from '@/components/Services';
 import Hours from '@/components/Hours';
+import Gallery from '@/components/Gallery';
 import Reviews from '@/components/Reviews';
 import Contact from '@/components/Contact';
 import Footer from '@/components/Footer';
@@ -17,6 +18,7 @@ export default function Home() {
         <About />
         <Services />
         <Hours />
+        <Gallery />
         <Reviews />
         <Contact />
       </main>
